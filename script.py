0ddc1cec28ad00cbb168fae272a0ff486ab79080
Anuj Singh Negi <anujnegi157@gmail.com>
2024-08-30 13:25:58 +0530

    bitto aggarwal

--- script.py
+++ script.py
@@ -204,6 +204,7 @@ actor_dict = {
 
 # Streamlit app layout
 st.title("Booking Service with AI")
+st.subheader("By Bittoo Aggarwal")
 
 customer_name = st.text_input("What's your name?", "Saurabh")
 phone_number = st.text_input("What's your phone number?", "+91 ")
